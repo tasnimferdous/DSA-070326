9e08c570a71fbbbd9b1b9ecfdf14c331155411c1
tasnimferdous <tasnimferdous216@gmail.com>
2026-03-08 00:57:14 +0600

    1d & 2d array

---
+++ src/Arrays/SimpleArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SimpleArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int size = in.nextInt();
+        int[] intArr = new int[size];
+        for(int i = 0; i < size; i++){
+            intArr[i] = i*5;
+        }
+
+        System.out.println(Arrays.toString(intArr));
+    }
+}
+
+
+
+
