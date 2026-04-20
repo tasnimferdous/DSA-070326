ba0fd13ede30aa3d5efda7681f7c7f13d19f68c2
tasnimferdous <tasnimferdous216@gmail.com>
2026-04-20 11:43:57 +0600

    merge sort algo

---
+++ src/Recursion/MergeSort.java
@@ -0,0 +1,49 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {6,5,4,3,2,1,0,-1};
+        System.out.println(Arrays.toString(mergeSort(arr)));
+    }
+
+    static int[] mergeSort(int[] arr){
+        if(arr.length <= 1) return arr;
+        int mid = arr.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] arr = new int[left.length + right.length];
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                arr[k] = left[i];
+                i++;
+            }else{
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < left.length){
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j < right.length){
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+
+        return arr;
+    }
+}
