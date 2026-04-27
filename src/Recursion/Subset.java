c0b64c1ddf6af3bdb2b55387021be9f94a88b4b3
tasnimferdous <tasnimferdous216@gmail.com>
2026-04-27 11:32:01 +0600

    subset list recursion

--- src/Recursion/Subset.java
+++ src/Recursion/Subset.java
@@ -8,21 +8,21 @@ public class Subset {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         List<List<Integer>> ans = subset(arr);
-//        List<List<Integer>> ans2 = subsetRec(new ArrayList<>() {}, arr);
+        List<List<Integer>> ans2 = subsetRec(new ArrayList<>() {}, arr);
         System.out.println(ans);
-//        System.out.println(ans2);
+        System.out.println(ans2);
     }
 
     private static List<List<Integer>> subsetRec(List<Integer> sub, int[] arr) {
-        List<List<Integer>> outer = new ArrayList<>();
         if(arr.length == 0){
-            outer.add(sub);
-            return outer;
+            List<List<Integer>> ans = new ArrayList<>();
+            ans.add(new ArrayList<>(sub));
+            return ans;
         }
-        int first = arr[0];
-        List<List<Integer>> left = subsetRec(sub, Arrays.copyOfRange(arr, 1, arr.length));
-        sub.add(first);
-        List<List<Integer>> right = subsetRec(sub, Arrays.copyOfRange(arr, 1, arr.length));
+        List<List<Integer>> left = new ArrayList<>(subsetRec(sub, Arrays.copyOfRange(arr, 1, arr.length)));
+        sub.add(arr[0]);
+        List<List<Integer>> right = new ArrayList<>(subsetRec(sub, Arrays.copyOfRange(arr, 1, arr.length)));
+        sub.removeLast();
         left.addAll(right);
         return left;
     }
