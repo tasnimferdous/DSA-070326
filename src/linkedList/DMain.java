97e250b6350249f99f16b85a0aef5237a6a6ba83
tasnimferdous <tasnimferdous216@gmail.com>
2026-05-06 10:54:28 +0600

    Doubly Linked List Operations

---
+++ src/linkedList/DMain.java
@@ -0,0 +1,14 @@
+package linkedList;
+
+public class DMain {
+    public static void main(String[] args) {
+        DLL list = new DLL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(8, 65);
+        list.display();
+    }
+}
